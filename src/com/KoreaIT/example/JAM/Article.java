d104ab2808f87825f7b77ec8ab86ad99fe45ae74
dope66 <prince628@naver.com>
2023-01-04 22:48:49 +0900

    기본적인 게시물 작성, 리스트 구현

--- src/com/KoreaIT/example/JAM/Article.java
+++ src/com/KoreaIT/example/JAM/Article.java
@@ -4,5 +4,9 @@ public class Article {
 	int id ;
 	String title;
 	String body;
-	
+	public Article(int id , String title , String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
 }
