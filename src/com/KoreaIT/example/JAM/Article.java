49b9010e3ad471821e8ff7951b724c134d190610
dope66 <prince628@naver.com>
2023-01-06 21:54:40 +0900

    DBUtil 활용 소스코드정리

--- src/com/KoreaIT/example/JAM/Article.java
+++ src/com/KoreaIT/example/JAM/Article.java
@@ -1,9 +1,12 @@
 package com.KoreaIT.example.JAM;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 public class Article {
 	int id ;
-	String regDate;
-	String updateDate;
+	LocalDateTime regDate;
+	LocalDateTime updateDate;
 	String title;
 	String body;
 	
@@ -13,13 +16,20 @@ public class Article {
 		this.title = title;
 		this.body = body;
 	}
-	public Article(int id, String regDate, String updateDate, String title, String body) {
+	public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.updateDate = updateDate;
 		this.title = title;
 		this.body = body;
 		
+	}
+	public Article(Map<String, Object> articleMap) {
+		this.id= (int) articleMap.get("id");
+		this.regDate= (LocalDateTime) articleMap.get("regDate");
+		this.updateDate = (LocalDateTime) articleMap.get("updateDate");
+		this.title= (String) articleMap.get("title");
+		this.body= (String) articleMap.get("body");
 	}
 	@Override
 	public String toString() {
